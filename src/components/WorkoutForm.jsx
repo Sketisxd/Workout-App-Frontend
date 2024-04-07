85e21725571fc9b2965933bd4698b2a71e03f56c
Sketisxd <mepicutis@gmail.com>
2024-04-08 00:46:21 +0300

    second comit

--- src/components/WorkoutForm.jsx
+++ src/components/WorkoutForm.jsx
@@ -13,7 +13,7 @@ const WorkoutForm = () => {
 
         const workout = { title, load, reps }
 
-        const response = await fetch('/api/workouts', {
+        const response = await fetch('https://workout-app-backend-1x4w.onrender.com/api/workouts', {
             method: 'POST',
             body: JSON.stringify(workout),
             headers: {
